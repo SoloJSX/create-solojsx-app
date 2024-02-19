8ed7c149049d390b0cf6eeaf960a7fc1b305428a
ee <e2gamedev@outlook.com>
2024-02-19 18:26:15 +0100

    fixed coloring on terminal output

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const { execSync } = require('child_process');
-const path = require('path');
+#!/usr/bin/env node
+import chalk from 'chalk';
+import { execSync } from 'child_process';
+import { join } from 'path';
 
-// Get the project name from the command line arguments
 const projectName = process.argv[2];
-
 if (!projectName) {
     console.error('Please provide a project name');
     process.exit(1);
 }
+const projectPath = join(process.cwd(), projectName);
+try {
+    execSync(`git clone https://github.com/eekelof/SoloJSX-template-ts.git ${projectPath}`);
+    process.chdir(projectPath);
+    execSync('npm i');
+}
+catch (err) {
+    console.log(chalk.red("Directory", projectPath, "already exists, or something else went wrong."));
+    process.exit(1);
+}
 
-// Define the path where the project should be cloned
-const projectPath = path.join(process.cwd(), projectName);
-
-// Clone the template into the project path
-execSync(`git clone https://github.com/eekelof/SoloJSX-template-ts.git ${projectPath}`);
+console.log(chalk.green("Project", chalk.green.bold(projectName), "created!"));
+console.log(chalk.blue("Now run the following commands:"));
+console.log(`cd ${projectName}`);
+console.log(`npm run dev`);
